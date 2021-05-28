37ec09eccd076360daeb643a7b7d1559ba643c17
haethehae <haethehae>
2021-05-28 19:42:33 +0900

    Initial Commit

---
+++ Theme Creator/Theme_CreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Theme_CreatorApp.swift
+//  Theme Creator
+//
+//  Created by Hae Chan Yoon on 2021/05/28.
+//
+
+import SwiftUI
+
+@main
+struct Theme_CreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
